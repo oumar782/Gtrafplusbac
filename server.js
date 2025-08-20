ca0af7d1651e14a3ce1fb21da3c4ba5cd2ed15bc
Oumar Diane <oumardiane399@gmail.com>
2025-08-20 23:06:35 +0200

    first commit12

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://g-traf.vercel.app/",
-      "https://admingtraf.vercel.app/"
+      "https://g-traf.vercel.app",
+      "https://admingtraf.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
